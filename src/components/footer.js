9f394a50edbcf2ccabf8917dcbc28e22fe1faa1f
Leora Wenger <web@leoraw.com>
2019-03-10 12:39:50 -0400

    social react icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons';
+import styled from 'styled-components'
 
-//import Image from "../components/image"
-
+const Social = styled.div`
+        padding: 0 5px;
+  `
 const Footer = () => {
   return (
-    <div>
-      social media apps - needs image and link - do this in json?
-      https://github.com/react-icons/react-icons
-      <div style={{ display: `flex` }}>
+      <div style={{ 
+        display: `flex`,
+        justifyContent: 'center',
+        paddingBottom: 10
+       }}>
+      <Social><SocialIcon url="https://github.com/leorawe" /></Social>
+      <Social><SocialIcon url="https://twitter.com/leoraw" /></Social>
+      <Social><SocialIcon url="https://codepen.com/leoraw" /></Social>
       {/* <p><a href="https://codepen.io/leoraw/"><img src="../images/social/codepen-50px.jpg" alt="CodePen"></a> &nbsp; <a href="https://twitter.com/leoraw"><img src="../images/social/twitter-icon.png" alt="Leora Wenger on Twitter"></a> &nbsp; <a href="http://www.pinterest.com/leorawenger/"><img src="../images/social/pinterest.png" alt="Pinterest" /></a> &nbsp; <a href="http://www.linkedin.com/in/leoraw/"><img src="../images/social/linkedin.png" alt="Leora Wenger on LinkedIn" /></a> &nbsp; <a href="https://github.com/leorawe"><img src="../images/social/github-mark-50px.png" alt="GitHub" /></a></p></div>
         */} 
         </div>
-    </div>
   )
 }
 
